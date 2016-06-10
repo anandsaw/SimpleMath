98310b25ba9799f629eb2e80a8c8509ab7d3f967
Anand Sawant <anandsaw@web103094.wireless.mcgill.ca>
2016-06-10 15:19:42 -0400

    Add java files.

---
+++ simple-math/src/main/java/division/Divider.java
@@ -0,0 +1,9 @@
+package division;
+
+public class Divider {
+	
+	public double divide(int a, int b) {
+		return (double) a / b;
+	}
+
+}
---
+++ simple-math/src/main/java/multiplication/Multiplier.java
@@ -0,0 +1,9 @@
+package multiplication;
+
+public class Multiplier {
+	
+	public int multiply(int a , int b) {
+		return a * b;
+	}
+
+}
