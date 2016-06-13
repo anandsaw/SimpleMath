2e990f7062ef3b12c84d4bdcec6a179eb22b6c65
Anand Sawant <anandsaw@Anands-MacBook-Pro.local>
2016-06-12 21:27:27 -0400

    Check of division by 0.

--- src/main/java/division/Divider.java
+++ src/main/java/division/Divider.java
@@ -1,9 +1,13 @@
 package division;
 
 public class Divider {
-	
+
 	public double divide(int a, int b) {
-		return (double) a / b;
+		if (b != 0) {
+			return (double) a / b;
+		} else {
+			return Double.MAX_VALUE;
+		}
 	}
 
 }
